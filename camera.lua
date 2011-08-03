30905becd525504d025c7b0ba4b936583714a341
josh <jbothun@concentricsky.com>
2011-08-02 21:19:11 -0700

    improved camera to use the new interpolator object

--- camera.lua
+++ camera.lua
@@ -7,50 +7,49 @@
 
 require 'leaf.object'
 require 'leaf.vector'
+require 'leaf.timer'
 
 leaf.camera = leaf.Object:new()
 local camera = leaf.camera
 
 camera.zoom 		= 1.0
-camera.zoom_target	= 1.0
 camera.zoom_speed 	= 1.15
 camera.zoom_min 	= 0.2
 camera.zoom_max 	= 5.0
-camera.interp_alpha = 1.0
 
 camera.position		= leaf.Vector:new()
 
 -- This must be overrided to the tracking target vector object you want!
 camera.target		= leaf.Vector:new()
 
+--- Pass a vector table, or any table with 'x' and 'y' fields, for the camera to track
+function camera.track(target)
+	camera.target = target
+end
+
+--- Must be called in main loop
 function camera.update(dt)
-	-- Linearly interpolate towards target zoom level
-	if camera.interp_alpha < 1.0 then
-		camera.interp_alpha = camera.interp_alpha + dt
-		camera.zoom = camera.zoom + (camera.zoom_target - camera.zoom) * camera.interp_alpha
-	else
-		camera.zoom = camera.zoom_target
-	end
-	
-	-- Center on target vector object
+	-- Center camera position on target table's x and y fields
 	camera.position.x = target.x - love.graphics.getWidth() * camera.zoom / 2.0
 	camera.position.y = target.y - love.graphics.getHeight() * camera.zoom / 2.0
 end
 
-function camera.zoomin()
-	-- Begin interpolating
-	camera.interp_alpha = 0.0
-	camera.zoom_target = camera.zoom_target / camera.zoom_speed
-	if camera.zoom_target < camera.zoom_min then
-		camera.zoom_target = camera.zoom_min
-	end
+function camera.zoomIn()
+	-- Create an interpolator to zoom in
+	local startval = camera.zoom
+	local endval = camera.zoom / camera.zoom_speed
+	time.interp(camera.zoom_speed, 
+				function (alpha)
+					camera.zoom = startval + alpha * (endval - startval)
+				end)
 end
 
-function camera.zoomout()
-	-- Begin interpolating
-	camera.interp_alpha = 0.0
-	camera.zoom_target = camera.zoom_target * camera.zoom_speed
-	if camera.zoom_target > camera.zoom_max then
-		camera.zoom_target = camera.zoom_max
-	end
+function camera.zoomOut()
+	-- Create an interpolator to zoom out
+	local startval = camera.zoom
+	local endval = camera.zoom * camera.zoom_speed
+	time.interp(camera.zoom_speed, 
+				function (alpha)
+					camera.zoom = startval + alpha * (endval - startval)
+				end)
 end
