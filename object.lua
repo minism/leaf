405a2b66ef1bdb29bd79d5c34d13d717e182436a
josh <jbothun@concentricsky.com>
2011-11-08 04:18:54 -0800

    cleaned up object and vector

--- object.lua
+++ object.lua
@@ -1,17 +1,38 @@
--- ++++++++++++++++++++++++++++++
--- + Leaf Object Class          +
--- + Copyright 2011 Josh Bothun +
--- + joshbothun@gmail.com       +
--- + minornine.com              +
--- ++++++++++++++++++++++++++++++
+--[[
+
+########################################################################
+#                                                                      #
+# object.lua                                                           #
+#                                                                      #
+# Base object for all modules in leaf, and simple OO implementation.   #
+#                                                                      #
+# Copyright 2011 Josh Bothun                                           #
+# joshbothun@gmail.com                                                 #
+# http://minornine.com                                                 #
+#                                                                      #
+# This program is free software: you can redistribute it and/or modify #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# This program is distributed in the hope that it will be useful,      #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+# GNU General Public License <http://www.gnu.org/licenses/> for        #
+# more details.                                                        #
+#                                                                      #
+########################################################################
+
+--]]        
 
 leaf.Object = {}
 local Object = leaf.Object
 
+-- Repeated in extend, but needed for base
 Object.__index = Object
 Object.__call = function(c, ...) return c:new(...) end
 
---- Creates, initializes, and returns a new object
+-- Creates, initializes, and returns a new object
 function Object:new(...)
 	local obj = {}
 	setmetatable(obj, self)
@@ -19,10 +40,11 @@ function Object:new(...)
 	return obj
 end
 
---- Creates and returns a new class
+-- Creates and returns a new class
 function Object:extend(classname)
     local class = {}
     setmetatable(class, self)
+    class._super = self
     -- Inherit metamethods
     for k, v in pairs(self) do
         if k:match('^__') then
@@ -34,15 +56,15 @@ function Object:extend(classname)
     return class
 end
 
---- Returns the type name of the object
-function Object:type()
-    if type(self) == 'table' and self._type then
-        return self._type
-    else
-        return type(self)
-    end
-end
-
-----[[ Abstract methods ]]----
+-- Abstract methods --
 
 function Object:init(...) end
+
+-- Global functions --
+
+-- Check if an object is an instance of its prototype
+
+function isinstance(obj, class)
+    return getmetatable(obj) == class
+end
+
--- vector.lua
+++ vector.lua
@@ -1,44 +1,124 @@
--- ++++++++++++++++++++++++++++++
--- + Leaf Vector Class          +
--- + Copyright 2011 Josh Bothun +
--- + joshbothun@gmail.com       +
--- + minornine.com              +
--- ++++++++++++++++++++++++++++++
+--[[
+
+#########################################################################
+#                                                                       #
+# vector.lua                                                            #
+#                                                                       #
+# 2D vector class                                                       #
+#                                                                       #
+# Copyright 2011 Josh Bothun                                            #
+# joshbothun@gmail.com                                                  #
+# http://minornine.com                                                  #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License <http://www.gnu.org/licenses/> for         #
+# more details.                                                         #
+#                                                                       #
+#########################################################################                                                    
+
+--]]
 
 require 'leaf.object'
 require 'math'
 
-leaf.Vector = leaf.Object:extend('Vector')
+leaf.Vector = leaf.Object:extend()
 local Vector = leaf.Vector
 
+local function assert_vecs(a, b)
+	assert(isinstance(a, Vector) and isinstance(b, Vector), "Operands must be Vectors.")
+end
+
 function Vector:init(x, y)
-    self.x = x or 0.0
-    self.y = y or 0.0
+    self.x = x or 0
+    self.y = y or 0
 end
 
-function Vector.__add(v1, v2)
-	local result = Vector:new()
-	result.x = v1.x + v2.x
-	result.y = v1.y + v2.y
-	return result
+function Vector.__add(a, b)
+	assert_vecs(a, b)
+	return Vector(a.x + b.x, a.y + b.y)
 end
 
-function Vector.__mul(vec, scalar)
-	local result = Vector:new()
-	result.x = vec.x * scalar
-	result.y = vec.y * scalar
-	return result
+function Vector.__sub(a, b)
+	assert_vecs(a, b)
+	return Vector(a.x - b.x, a.y - b.y)
 end
 
-function Vector:length()
+function Vector.__mul(a, b)
+	if type(a) == 'number' then
+		assert(isinstance(b, Vector), "Operands must be numbers or vectors.")
+		return Vector(b.x * a, b.y * a)
+	elseif type(b) == 'number' then
+		assert(isinstance(a, Vector), "Operands must be numbers or vectors.")
+		return Vector(a.x * b, a.y * b)
+	else
+		assert_vecs(a, b)
+		return Vector(a.x * b.x, a.y * b.y)
+	end
+end
+
+function Vector.__div(a, b)
+	assert(isinstance(a, Vector) and type(b) == 'number', "Can only divide a Vector by a number.")
+	return Vector(a.x / b, a.y / b)
+end
+
+function Vector.__eq(a, b)
+	assert_vecs(a, b)
+	return a.x == b.x and a.y == b.y
+end
+
+function Vector.__lt(a, b)
+	assert_vecs(a, b)
+	return a:len() < b:len()
+end
+
+function Vector.__lte(a, b)
+	assert_vecs(a, b)
+	return a:len() <= b:len()
+end
+
+function Vector.__gt(a, b)
+	assert_vecs(a, b)
+	return a:len() > b:len()
+end
+
+function Vector.__gte(a, b)
+	assert_vecs(a, b)
+	return a:len() >= b:len()
+end
+
+function Vector:__tostring()
+	return "(" .. self.x .. "," .. self.y .. ")"
+end
+
+function Vector:copy()
+	return Vector(self.x, self.y)
+end
+
+function Vector:unpack()
+	return self.x, self.y
+end
+
+function Vector:len()
 	return math.sqrt(self.x * self.x, self.y * self.y)
 end
 
-function Vector:clear()
+function Vector:normalize()
+	return self / self:len()
+end
+
+function Vector:rotate(theta)
+	return Vector(self.x * math.cos(theta) - self.y * math.sin(theta),
+				  self.x * math.sin(theta) + self.y * math.cos(theta))
+end
+
+function Vector:reset()
 	self.x = 0
 	self.y = 0
 end
-
-
----- Shortcuts ----
-Vector.len = Vector.length
