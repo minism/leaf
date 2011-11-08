568a8608298f0f4cda9e2c3f463e31f5f81b863e
josh <jbothun@concentricsky.com>
2011-11-08 05:41:21 -0800

    updated header on loader

--- loader.lua
+++ loader.lua
@@ -1,9 +1,29 @@
--- ++++++++++++++++++++++++++++++
--- + Leaf Loader Module         +
--- + Copyright 2011 Josh Bothun +
--- + joshbothun@gmail.com       +
--- + minornine.com              +
--- ++++++++++++++++++++++++++++++
+--[[
+
+#########################################################################
+#                                                                       #
+# loader.lua                                                            #
+#                                                                       #
+# Love2D filesystem loading functions                                   #
+#                                                                       #
+# Copyright 2011 Josh Bothun                                            #
+# joshbothun@gmail.com                                                  #
+# http://minornine.com                                                  #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License <http://www.gnu.org/licenses/> for         #
+# more details.                                                         #
+#                                                                       #
+#########################################################################      
+
+--]]                                
 
 leaf.loader = {}
 local loader = leaf.loader
@@ -23,4 +43,3 @@ function loader.loadImages(dir)
 	end
     return map
 end
-
