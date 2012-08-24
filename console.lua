7233096795e105c5598a7798a473b4c1ecaa275e
josh <jbothun@concentricsky.com>
2012-08-23 19:18:45 -0700

    add basic input parsing to console

--- console.lua
+++ console.lua
@@ -39,7 +39,7 @@ local PADDING = 10
 local Message = leaf.Object:extend()
 
 function Message:init(data, level)
-    self.data = data
+    self.data = data or ""
     self.level = level or 'info'
 end
 
@@ -50,9 +50,12 @@ local Console = leaf.Context:extend()
 function Console:init()
     self.font = love.graphics.newFont(10)
     self.queue = leaf.Queue(HISTORY)
+    self.input = ""
     self.color = leaf.ColorPalette {
         info = {255, 255, 255},
+        input = {255, 255, 255},
         error = {255, 0, 0},
+        overlay = {0, 0, 0},
     }
 end
 
@@ -67,30 +70,82 @@ function Console:error(data)
 end
 
 function Console:draw()
-    self:drawInput()
-    self:drawLog()
-end
+    local width, height = love.graphics.getWidth(), love.graphics.getHeight()
 
-function Console:drawInput()
+    -- Draw log
+    self:drawLog(width, height)
 
-end
-
-function Console:drawLog()
-    local width = love.graphics.getWidth()
-    local height = love.graphics.getHeight()
-    love.graphics.setColor(0, 0, 0, 128)
+    -- Draw overlay
+    self.color.overlay(100)
     love.graphics.rectangle('fill', 0, 0, width, height)
+
+    -- Draw input
+    self.color.input()
+    love.graphics.print(self.input .. '|', PADDING, height - PADDING - self.font:getHeight())
+end
+
+function Console:drawLog(width, height)
+    local width = width or love.graphics.getWidth()
+    local height = height or love.graphics.getHeight()
     love.graphics.setColor(255, 255, 255)
     love.graphics.setFont(self.font)
     for i, message in self.queue:iter_reverse() do
         self.color[message.level]()
         love.graphics.printf(message.data, PADDING, 
-                             height - PADDING - i * self.font:getHeight(), 
+                             height - PADDING - (i + 1) * self.font:getHeight(), 
                              width, 'left')
     end
 end
 
+
+-- Console input handling functions
+local shift_map = {
+    ['9'] = '(',
+    ['0'] = ')',
+    ['['] = '{',
+    [']'] = '}',
+    [';'] = ':',
+}
+
+local ignored_keys = leaf.Set {
+    'lshift',
+    'rshift',   
+}
+
+function charMap (key)
+    if love.keyboard.isDown('lshift') or love.keyboard.isDown('rshift') then
+        if shift_map[key] then
+            return shift_map[key]
+        end
+    end
+    return key
+end
+
 function Console:keypressed(key, unicode)
+    if key == 'escape' then
+        self:clearInput()
+    elseif key == 'return' then
+        self:submitInput()
+    elseif key == 'backspace' then
+        self.input = string.sub(self.input, 1, -2)
+    elseif not ignored_keys:contains(key) then
+        self.input = self.input .. charMap(key)
+    end
+end
+
+function Console:clearInput()
+    self.input = ""
+end
+
+function Console:submitInput()
+    -- Attempt to parse input as lua
+    chunk, error = loadstring(self.input)
+    if error then
+        self:error(error)
+    else
+        chunk()
+    end
+    self:clearInput()
 end
 
 -- Namespace exports
